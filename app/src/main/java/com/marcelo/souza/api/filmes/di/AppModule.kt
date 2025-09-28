e69a6cb1bb4b0bc99ea6ea2196665cdbb48a4628
Marcelo Souza <developermarcelosouza@gmail.com>
2025-09-27 22:00:49 -0300

    Configurando o setup do projeto (#3)
    Configuração das camadas do projeto e configuração das bibliotecas a
    serem utilizadas e build.gradle do projeto

---
+++ app/src/main/java/com/marcelo/souza/api/filmes/di/AppModule.kt
@@ -0,0 +1,8 @@
+package com.marcelo.souza.api.filmes.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("com.marcelo.souza.api.filmes")
+class AppModule
